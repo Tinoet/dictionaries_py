345ae2f28824683fa0f2a792fad482bd624bab16
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-29 18:05:17 +0100

    Add files via upload

---
+++ dictionaries_py/dictionaries.py
@@ -0,0 +1,11 @@
+phone = input("phone: ")
+digital_mapping = {
+  "1": "one",
+  "2": "two",
+  "3": "three",
+  "4":"four"
+}
+output= ""
+for ch in phone:
+  output += digital_mapping.get(ch, '!') + ""
+print(output)
